d8fe4751bae9206e8b174cc4cc57d2a53b43d5ac
Kaijun <kaijun_huang@outlook.com>
2023-03-09 16:58:34 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
+const exphbs = require("express-handlebars");
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.render("index");
 });
 
 app.listen(3000, () => {
