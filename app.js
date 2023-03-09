e3caaccd9c41322f386ca8ee9e36cdd9aea827ed
Kaijun <kaijun_huang@outlook.com>
2023-03-09 16:31:09 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+db.once("open", () => {
+  console.log("mongdb connected!");
+});
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
