fa6b44ef43ab78188b82b7fc6ee7bd41fb9c649f
Kaijun <kaijun_huang@outlook.com>
2023-03-11 20:54:37 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Todo = require("./models/todo");
+const bodyParser = require("body-parser");
 const db = mongoose.connection;
 db.on("error", () => {
   console.log("mongodb error!");
@@ -18,9 +19,10 @@ mongoose.connect(process.env.MONGODB_URI, {
 });
 
 const exphbs = require("express-handlebars");
-
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   Todo.find()
@@ -29,6 +31,18 @@ app.get("/", (req, res) => {
     .catch((error) => console.error(error));
 });
 
+app.get("/todos/new", (req, res) => {
+  return res.render("new");
+});
+app.post("/todos", (req, res) => {
+  const name = req.body.name;
+  return Todo.create({ name })
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log("error"));
+  // return Todo.create({ name })
+  //   .then(() => res.redirect("/"))
+  //   .catch((error) => console.error("error"));
+});
 app.listen(3000, () => {
   console.log("App is running on port 3000");
 });
